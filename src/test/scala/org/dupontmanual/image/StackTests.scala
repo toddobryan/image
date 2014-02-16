99c8070f635ea416852bd2adc4283d3c6b4be106
Todd O'Bryan <toddobryan@gmail.com>
2014-02-16 16:15:38 -0500

    added a placeImage method that allows placing an image at a particular point on a background

--- src/test/scala/org/dupontmanual/image/StackTests.scala
+++ src/test/scala/org/dupontmanual/image/StackTests.scala
@@ -5,6 +5,8 @@ import org.scalatest.{ FunSuite, Matchers }
 class StackTests extends FunSuite with Matchers {
   val e1 = EllipseFilled(Color.DeepSkyBlue, 70, 40)
   val e2 = EllipseFilled(Color.DarkGray, 20, 80)
+  val bg = RectangleFilled(Color.Gray, 300, 200)
+  val c = CircleFilled(Color.Aqua, 25)
   
   test("centered") {
     e1.stackOn(e2) shouldEqual Bitmap.fromWorkspace("/stacked/e1-e2.png")
@@ -18,6 +20,23 @@ class StackTests extends FunSuite with Matchers {
     e1.stackOn(e2, XAlign.Left, YAlign.Bottom) shouldEqual Bitmap.fromWorkspace("/stacked/e1-e2-left-bottom.png")
     e2.stackOn(e1, XAlign.Center, YAlign.Top) shouldEqual Bitmap.fromWorkspace("/stacked/e2-e1-center-top.png")
     e1.stackOn(e2, XAlign.Right, YAlign.Center) shouldEqual Bitmap.fromWorkspace("/stacked/e1-e2-right-center.png")
+  }
+  
+  test("placeImage") {
+    bg.placeImage(c, 150, 100) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-centered.png")
+    bg.placeImage(c, 150, 100, XAlign.Left, YAlign.Top) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-top-left.png")
+    bg.placeImage(c, 150, 100, XAlign.Right, YAlign.Center) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-center-right.png")
+    bg.placeImage(c, 150, 100, XAlign.Center, YAlign.Bottom) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-bottom-center.png")
+    // completely off-screen should be same object
+    bg.placeImage(c, -25, -25) should be theSameInstanceAs bg
+    bg.placeImage(c, 150, -30) should be theSameInstanceAs bg
+    bg.placeImage(c, 325, 100) should be theSameInstanceAs bg
+    bg.placeImage(c, 150, 225) should be theSameInstanceAs bg
+    // partially off-screen checks
+    bg.placeImage(c, 0, 0) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-center-at-0,0.png")
+    bg.placeImage(c, 150, 190, XAlign.Center, YAlign.Top) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-top-at-150,190.png")
+    bg.placeImage(c, 5, 100, XAlign.Right, YAlign.Center) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-right-at-5,100.png")
+    bg.placeImage(c, 275, 25, XAlign.Left, YAlign.Bottom) shouldEqual Bitmap.fromWorkspace("/stacked/bg-c-bottom-left-at-275,25.png")    
   }
 
 }
